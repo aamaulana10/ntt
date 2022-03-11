03ce4be5f6abd0bf22bd64c8a2c85af1e9562e05
aamaulana10 <maulana.yusuf@misteraladin.com>
2022-03-11 13:28:26 +0700

    Initial Commit

---
+++ NTT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NTT
+//
+//  Created by aa maulana10 on 11/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
